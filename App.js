f3a10247c51b08069c21155cc12a9815c2173f62
Merve <gulineyel@gmail.com>
2020-10-20 08:19:23 +0300

    project initialize

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    
+      <SafeAreaView>
+        <View>
+          <Text>
+            {/* Let's start 💥💥💥 */}
+          </Text>
+        </View>
+      </SafeAreaView>
+
+  );
+};
+
+
+
+export default App;
