f0ba7a1ca2720640b6f6e288f0dfcb0e9146eeeb
Merve <gulineyel@gmail.com>
2020-10-23 13:50:23 +0300

    RestaurantPahe.js

--- src/pages/RestaurantPage.js
+++ src/pages/RestaurantPage.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { View, Image, Text, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import {
+  View,
+  Image,
+  Text,
+  TouchableOpacity,
+  Dimensions,
+  StyleSheet,
+} from 'react-native';
 
-const RestaurantPage = () => {
-    return (
-        <View>
-            {/* <Image
-                style = {styles.img}
-                // source = {{uri:restaurant.image}}
-            /> */}
-            <Text>Ayse</Text>
-        </View>
-    )
-}
+const RestaurantPage = ({route, navigation}) => {
+  const {name, image, address} = route.params;
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.img} source={{uri: image}} />
+      <View style={{margin: 20}}>
+        <Text style={styles.restaurantName}>{name}</Text>
+        <TouchableOpacity>
+          <Text style={styles.restaurantAdress}>{address}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.restaurantAdress}>{address}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.restaurantAdress}>{address}</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 export default RestaurantPage;
 
-// const styles = StyleSheet.create({
-//     img:{
-//         width:Dimensions.get('window').width / 4
-//     }
-// })
+const styles = StyleSheet.create({
+  img: {
+    alignSelf: 'center',
+    width: Dimensions.get('window').width * 0.9,
+    height: 200,
+  },
+  restaurantName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  restaurantAdress: {},
+});
